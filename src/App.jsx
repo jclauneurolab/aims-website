0d9b5a928fee74d8dfe4a27662f37bf800449110
Mackenzie Snyder <masnyder2@hotmail.com>
2025-03-28 09:24:24 -0400

    Local changes for gh-pages build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './nav-bar/nav-bar';
 import HomePage from './home-page/home-page';
 import PeoplePage from './people-page/people-page';
--- src/nav-bar/nav-bar.jsx
+++ src/nav-bar/nav-bar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './nav-bar.css';
 import logo from '/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -11,22 +11,22 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-left">
-                <a href="/" className="logo">
+                <Link to="/" className="logo">
                     <img src={logo} alt="Logo" className="logo-image" />
-                </a>
+                </Link>
             </div>
             <div className="navbar-right">
                 <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>
                     <FontAwesomeIcon icon={faBars} className="menu-icon" />
                 </button>
                 <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/people">People</a></li>
-                    <li><a href="/papers">Papers</a></li>
-                    <li><a href="/news">News</a></li>
-                    <li><a href="/projects">Projects</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/people">People</Link></li>
+                    <li><Link to="/papers">Papers</Link></li>
+                    <li><Link to="/news">News</Link></li>
+                    <li><Link to="/projects">Projects</Link></li>
                     <li className="contact-button">
-                        <a href="/contact">Contact</a>
+                        <Link to="/contact">Contact</Link>
                     </li>
                 </ul>
             </div>
@@ -35,4 +35,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
